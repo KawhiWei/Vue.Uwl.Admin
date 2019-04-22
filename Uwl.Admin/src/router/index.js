83c3e174f9e6673fc933e389aeaace746313ceef
Aveuy <1585955375@qq.com>
2019-04-22 12:57:01 +0800

    创建一个新的前端项目，之前的太乱了，

---
+++ Uwl.Admin/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+      meta:{
+        title:'HelloWorld',
+        requireAuth:false,//表示此页面打开是否需要登录
+        NoNeedHome:true,//添加此字段表示不需要Home模板
+      },
+      hidden:true,
+    },
+    
+  ]
+})
