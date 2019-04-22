7266391c6205f695e2be06668dcb0be2cc6e4c65
Aveuy <1585955375@qq.com>
2019-04-22 17:49:38 +0800

    添加登录界面

--- Uwl.Admin/src/main.js
+++ Uwl.Admin/src/main.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Store from '@/Vuex/Store'//在全局Main.js中引入Store仓库
+import iview from 'iview' //引入Iview前端UI框架
 
 Vue.config.productionTip = false
 
+Vue.use(iview) //挂载iview框架
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- Uwl.Admin/src/router/index.js
+++ Uwl.Admin/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/Views/login.vue'
 
 
 Vue.use(Router)
@@ -15,9 +15,9 @@ export default new Router({
       meta:{
         title:'HelloWorld',
         requireAuth:false,//表示此页面打开是否需要登录
-        NoNeedHome:true,//添加此字段表示不需要Home模板
+        NoNeedHome:false,//添加此字段表示不需要Home模板
       },
-      hidden:true,
+      //hidden:true,
     },
     
   ]
