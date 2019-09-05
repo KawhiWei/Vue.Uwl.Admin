33ebd672e6dbf2eaedb34e222845595e554970ed
Aveuy <1585955375@qq.com>
2019-09-05 18:29:09 +0800

    init

--- Uwl.Admin/src/APIServer/Api.js
+++ Uwl.Admin/src/APIServer/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios' //在APi访问接口引入Vuex
 import router from '../router/index'
 import Vue from 'vue';
 import store from '../Vuex/store'
-var baseurl1="http://localhost:5001";//"http://139.199.219.154:8561";//
+var baseurl1="http://127.0.0.1:5001";//"http://139.199.219.154:8561";//
 
 
 
